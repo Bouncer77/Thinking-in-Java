45d22c52970bb6a48754caf0fa09ddf36d2966dc
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-07-28 18:49:44 +0300

    Глава 3

---
+++ chapter3/src/main/java/com/bouncer77/thinking/chapter3/App.java
@@ -0,0 +1,12 @@
+package com.bouncer77.thinking.chapter3;
+
+/**
+ * @author Kosenkov Ivan 28.07.2020
+ * */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+
+    }
+}
